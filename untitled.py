7015dd6f1b9d78e42b2a9e288adfb6d477329dbb
M-i-d-e772 <123970482+M-i-d-e772@users.noreply.github.com>
2023-02-21 17:06:54 +0100

    Add files via upload

---
+++ untitled.py
@@ -0,0 +1,8 @@
+is_on_instagram = True
+likes = 50
+views = 100
+
+if likes >= views/2 or views == likes and is_on_instagram:
+    print("you don blow")
+else :
+      print("you still never blow ooo")
