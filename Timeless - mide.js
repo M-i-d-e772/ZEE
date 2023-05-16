4e8020cab0bd5b61e14a2163db982648910ace3e
M-i-d-e772 <123970482+M-i-d-e772@users.noreply.github.com>
2023-05-16 22:38:56 +0100

    Add files via upload

---
+++ Timeless - mide.js
@@ -0,0 +1,488 @@
+var music1 = new Audio('musicfile.mp3');
+var music2 = new Audio('musicfile2.mp3');
+var music3 = new Audio('musicfile3.mp3');
+var music4 = new Audio('musicfile4.mp3');
+var music5 = new Audio('musicfile5.mp3');
+var music6 = new Audio('musicfile6.mp3');
+function play() {
+    const el = document.querySelector('img')
+    const one = document.getElementById('one');
+    const two = document.getElementById('two');
+    const three = document.getElementById('three');
+    const four = document.getElementById('four');
+    const five = document.getElementById('five');
+    const six = document.getElementById('six');
+    const play = document.getElementById('play');
+    const trace = document.getElementById('trace');
+    const now_playing1 = document.getElementById('now_playing1');
+    const not_playing = document.getElementById('not_playing');
+    const playlist_1 = document.getElementById('playlist_1');
+    const playlist_2 = document.getElementById('playlist_2');
+    const playlist_3 = document.getElementById('playlist_3');
+    const playlist_4 = document.getElementById('playlist_4');
+    const playlist_5 = document.getElementById('playlist_5');
+    const playlist_6 = document.getElementById('playlist_6');
+    const playlist_7 = document.getElementById('playlist_7');
+    now_playing1.innerHTML = "UNAVAILABLE (feat. Musa Keys)";
+    not_playing.innerHTML = 'Now playing : ' + 'UNAVAILABLE (feat. Musa Keys)';
+    playlist_1.style.color = "red"
+    playlist_2.style.color = "white"
+    playlist_3.style.color = "white"
+    playlist_4.style.color = "white"
+    playlist_5.style.color = "white"
+    playlist_6.style.color = "white"
+    playlist_7.style.color = "white"
+    one.innerHTML = '>>>'
+    two.innerHTML = '2'
+    three.innerHTML = '3'
+    four.innerHTML = "4"
+    five.innerHTML = '5'
+    six.innerHTML = '6'
+    eight.innerHTML = '7'
+    trace.innerHTML = "Davido"
+    el.src = "Davido-–-Timeless-Album.jpg";
+    music1.play();
+    music2.pause();
+    music2.currentTime = 0;
+    music3.pause();
+    music3.currentTime = 0;
+    music4.pause();
+    music4.currentTime = 0;
+    music5.pause();
+    music5.currentTime = 0;
+    music6.pause();
+    music6.currentTime = 0;
+}
+function shuffle() {
+    const el = document.querySelector('img')
+    const one = document.getElementById('one');
+    const two = document.getElementById('two');
+    const three = document.getElementById('three');
+    const four = document.getElementById('four');
+    const five = document.getElementById('five');
+    const six = document.getElementById('six');
+    const trace = document.getElementById('trace');
+    const shuffle = document.getElementById('shuffle');
+    const now_playing1 = document.getElementById('now_playing1');
+    const not_playing = document.getElementById('not_playing');
+    const playlist_1 = document.getElementById('playlist_1');
+    const playlist_2 = document.getElementById('playlist_2');
+    const playlist_3 = document.getElementById('playlist_3');
+    const playlist_4 = document.getElementById('playlist_4');
+    const playlist_5 = document.getElementById('playlist_5');
+    const playlist_6 = document.getElementById('playlist_6');
+    const playlist_7 = document.getElementById('playlist_7');
+    now_playing1.innerHTML = 'FOR THE ROAD (official Audio)';
+    not_playing.innerHTML = 'Now playing : ' + 'FOR THE ROAD (official Audio)';
+    playlist_1.style.color = "white"
+    playlist_2.style.color = "white"
+    playlist_3.style.color = "white"
+    playlist_4.style.color = "white"
+    playlist_5.style.color = "red"
+    playlist_6.style.color = "white"
+    playlist_7.style.color = "white"
+    one.innerHTML = '1'
+    two.innerHTML = '2'
+    three.innerHTML = '3'
+    four.innerHTML = "4"
+    five.innerHTML = '>>>'
+    six.innerHTML = '6'
+    eight.innerHTML = '7'
+    trace.innerHTML = "Davido"
+    el.src = "Davido-–-Timeless-Album.jpg";
+    music1.pause();
+    music1.currentTime = 0;
+    music2.pause();
+    music2.currentTime = 0;
+    music3.pause();
+    music3.currentTime = 0;
+    music4.pause();
+    music4.currentTime = 0;
+    music5.play();
+    music6.pause();
+    music6.currentTime = 0;
+}
+function one() {
+    const el = document.querySelector('img')
+    const one = document.getElementById('one');
+    const two = document.getElementById('two');
+    const three = document.getElementById('three');
+    const four = document.getElementById('four');
+    const five = document.getElementById('five');
+    const six = document.getElementById('six');
+    const trace = document.getElementById('trace');
+    const now_playing1 = document.getElementById('now_playing1');
+    const not_playing = document.getElementById('not_playing');
+    const playlist_1 = document.getElementById('playlist_1');
+    const playlist_2 = document.getElementById('playlist_2');
+    const playlist_3 = document.getElementById('playlist_3');
+    const playlist_4 = document.getElementById('playlist_4');
+    const playlist_5 = document.getElementById('playlist_5');
+    const playlist_6 = document.getElementById('playlist_6');
+    const playlist_7 = document.getElementById('playlist_7');
+    now_playing1.innerHTML = "UNAVAILABLE (feat. Musa Keys)";
+    not_playing.innerHTML = 'Now playing : ' + 'UNAVAILABLE (feat. Musa Keys)';
+    playlist_1.style.color = "red"
+    playlist_2.style.color = "white"
+    playlist_3.style.color = "white"
+    playlist_4.style.color = "white"
+    playlist_5.style.color = "white"
+    playlist_6.style.color = "white"
+    playlist_7.style.color = "white"
+    one.innerHTML = '>>>'
+    two.innerHTML = '2'
+    three.innerHTML = '3'
+    four.innerHTML = "4"
+    five.innerHTML = '5'
+    six.innerHTML = '6'
+    eight.innerHTML = '7'
+    trace.innerHTML = "Davido"
+    el.src = "Davido-–-Timeless-Album.jpg";
+    music1.play();
+    music2.pause();
+    music2.currentTime = 0;
+    music3.pause();
+    music3.currentTime = 0;
+    music4.pause();
+    music4.currentTime = 0;
+    music5.pause();
+    music5.currentTime = 0;
+    music6.pause();
+    music6.currentTime = 0;
+}
+function two() {
+    const el = document.querySelector('img')
+    const one = document.getElementById('one');
+    const two = document.getElementById('two');
+    const three = document.getElementById('three');
+    const four = document.getElementById('four');
+    const five = document.getElementById('five');
+    const six = document.getElementById('six');
+    const trace = document.getElementById('trace');
+    const now_playing1 = document.getElementById('now_playing1');
+    const not_playing = document.getElementById('not_playing');
+    const playlist_1 = document.getElementById('playlist_1');
+    const playlist_2 = document.getElementById('playlist_2');
+    const playlist_3 = document.getElementById('playlist_3');
+    const playlist_4 = document.getElementById('playlist_4');
+    const playlist_5 = document.getElementById('playlist_5');
+    const playlist_6 = document.getElementById('playlist_6');
+    const playlist_7 = document.getElementById('playlist_7');
+    now_playing1.innerHTML = "KANTE (feat. Fave)";
+    not_playing.innerHTML = 'Now playing : ' + 'KANTE (feat. Fave)';
+    playlist_1.style.color = "white"
+    playlist_2.style.color = "red"
+    playlist_3.style.color = "white"
+    playlist_4.style.color = "white"
+    playlist_5.style.color = "white"
+    playlist_6.style.color = "white"
+    playlist_7.style.color = "white"
+    one.innerHTML = '1'
+    two.innerHTML = '>>>'
+    three.innerHTML = '3'
+    four.innerHTML = "4"
+    five.innerHTML = '5'
+    six.innerHTML = '6'
+    eight.innerHTML = '7'
+    trace.innerHTML = "Davido"
+    el.src = "Davido-–-Timeless-Album.jpg";
+    music1.pause();
+    music1.currentTime = 0;
+    music2.play();
+    music3.pause();
+    music3.currentTime = 0;
+    music4.pause();
+    music4.currentTime = 0;
+    music5.pause();
+    music5.currentTime = 0;
+    music6.pause();
+    music6.currentTime = 0;
+}
+function three() {
+    const el = document.querySelector('img')
+    const one = document.getElementById('one');
+    const two = document.getElementById('two');
+    const three = document.getElementById('three');
+    const four = document.getElementById('four');
+    const five = document.getElementById('five');
+    const six = document.getElementById('six');
+    const trace = document.getElementById('trace');
+    const now_playing1 = document.getElementById('now_playing1');
+    const not_playing = document.getElementById('not_playing');
+    const playlist_1 = document.getElementById('playlist_1');
+    const playlist_2 = document.getElementById('playlist_2');
+    const playlist_3 = document.getElementById('playlist_3');
+    const playlist_4 = document.getElementById('playlist_4');
+    const playlist_5 = document.getElementById('playlist_5');
+    const playlist_6 = document.getElementById('playlist_6');
+    const playlist_7 = document.getElementById('playlist_7');
+    now_playing1.innerHTML = "OVER DEM (Official Audio)";
+    not_playing.innerHTML = 'Now playing : ' + 'OVER DEM (Official Audio)';
+    playlist_1.style.color = "white"
+    playlist_2.style.color = "white"
+    playlist_3.style.color = "red"
+    playlist_4.style.color = "white"
+    playlist_5.style.color = "white"
+    playlist_6.style.color = "white"
+    playlist_7.style.color = "white"
+    one.innerHTML = '1'
+    two.innerHTML = '2'
+    three.innerHTML = '>>>'
+    four.innerHTML = "4"
+    five.innerHTML = '5'
+    six.innerHTML = '6'
+    eight.innerHTML = '7'
+    trace.innerHTML = "Davido"
+    el.src = "Davido-–-Timeless-Album.jpg";
+    music1.pause();
+    music1.currentTime = 0;
+    music2.pause();
+    music2.currentTime = 0;
+    music3.play();
+    music4.pause();
+    music4.currentTime = 0;
+    music5.pause();
+    music5.currentTime = 0;
+    music6.pause();
+    music6.currentTime = 0;
+}
+function four() {
+    const el = document.querySelector('img')
+    const one = document.getElementById('one');
+    const two = document.getElementById('two');
+    const three = document.getElementById('three');
+    const four = document.getElementById('four');
+    const five = document.getElementById('five');
+    const six = document.getElementById('six');
+    const trace = document.getElementById('trace');
+    const now_playing1 = document.getElementById('now_playing1');
+    const not_playing = document.getElementById('not_playing');
+    const playlist_1 = document.getElementById('playlist_1');
+    const playlist_2 = document.getElementById('playlist_2');
+    const playlist_3 = document.getElementById('playlist_3');
+    const playlist_4 = document.getElementById('playlist_4');
+    const playlist_5 = document.getElementById('playlist_5');
+    const playlist_6 = document.getElementById('playlist_6');
+    const playlist_7 = document.getElementById('playlist_7');
+    now_playing1.innerHTML = "U(JUJU) (feat. Skepta)";
+    not_playing.innerHTML = 'Now playing : ' + 'U(JUJU) (feat. Skepta)';
+    playlist_1.style.color = "white"
+    playlist_2.style.color = "white"
+    playlist_3.style.color = "white"
+    playlist_4.style.color = "red"
+    playlist_5.style.color = "white"
+    playlist_6.style.color = "white"
+    playlist_7.style.color = "white"
+    one.innerHTML = '1'
+    two.innerHTML = '2'
+    three.innerHTML = '3'
+    four.innerHTML = ">>>"
+    five.innerHTML = '5'
+    six.innerHTML = '6'
+    eight.innerHTML = '7'
+    trace.innerHTML = "Davido"
+    el.src = "Davido-–-Timeless-Album.jpg";
+    music1.pause();
+    music1.currentTime = 0;
+    music2.pause();
+    music2.currentTime = 0;
+    music3.pause();
+    music3.currentTime = 0;
+    music4.play();
+    music5.pause();
+    music5.currentTime = 0;
+    music6.pause();
+    music6.currentTime = 0;
+}
+function five() {
+    const el = document.querySelector('img')
+    const one = document.getElementById('one');
+    const two = document.getElementById('two');
+    const three = document.getElementById('three');
+    const four = document.getElementById('four');
+    const five = document.getElementById('five');
+    const six = document.getElementById('six');
+    const trace = document.getElementById('trace');
+    const now_playing1 = document.getElementById('now_playing1');
+    const not_playing = document.getElementById('not_playing');
+    const playlist_1 = document.getElementById('playlist_1');
+    const playlist_2 = document.getElementById('playlist_2');
+    const playlist_3 = document.getElementById('playlist_3');
+    const playlist_4 = document.getElementById('playlist_4');
+    const playlist_5 = document.getElementById('playlist_5');
+    const playlist_6 = document.getElementById('playlist_6');
+    const playlist_7 = document.getElementById('playlist_7');
+    now_playing1.innerHTML = "FOR THE ROAD (official Audio)";
+    not_playing.innerHTML = 'Now playing : ' + 'FOR THE ROAD (official Audio)';
+    playlist_1.style.color = "white"
+    playlist_2.style.color = "white"
+    playlist_3.style.color = "white"
+    playlist_4.style.color = "white"
+    playlist_5.style.color = "red"
+    playlist_6.style.color = "white"
+    playlist_7.style.color = "white"
+    one.innerHTML = '1'
+    two.innerHTML = '2'
+    three.innerHTML = '3'
+    four.innerHTML = "4"
+    five.innerHTML = '>>>'
+    six.innerHTML = '6'
+    eight.innerHTML = '7'
+    trace.innerHTML = "Davido"
+    el.src = "Davido-–-Timeless-Album.jpg";
+    music1.pause();
+    music1.currentTime = 0;
+    music2.pause();
+    music2.currentTime = 0;
+    music3.pause();
+    music3.currentTime = 0;
+    music4.pause();
+    music4.currentTime = 0;
+    music5.play();
+    music6.pause();
+    music6.currentTime = 0;
+}
+function six() {
+    const el = document.querySelector('img')
+    const one = document.getElementById('one');
+    const two = document.getElementById('two');
+    const three = document.getElementById('three');
+    const four = document.getElementById('four');
+    const five = document.getElementById('five');
+    const six = document.getElementById('six');
+    const trace = document.getElementById('trace');
+    const now_playing1 = document.getElementById('now_playing1');
+    const not_playing = document.getElementById('not_playing');
+    const playlist_1 = document.getElementById('playlist_1');
+    const playlist_2 = document.getElementById('playlist_2');
+    const playlist_3 = document.getElementById('playlist_3');
+    const playlist_4 = document.getElementById('playlist_4');
+    const playlist_5 = document.getElementById('playlist_5');
+    const playlist_6 = document.getElementById('playlist_6');
+    const playlist_7 = document.getElementById('playlist_7');
+    now_playing1.innerHTML = "LCND (Official Audio)";
+    not_playing.innerHTML = 'Now playing : ' + 'LCND (Official Audio)';
+    playlist_1.style.color = "white"
+    playlist_2.style.color = "white"
+    playlist_3.style.color = "white"
+    playlist_4.style.color = "white"
+    playlist_5.style.color = "white"
+    playlist_6.style.color = "red"
+    playlist_7.style.color = "white"
+    one.innerHTML = '1'
+    two.innerHTML = '2'
+    three.innerHTML = '3'
+    four.innerHTML = "4"
+    five.innerHTML = '5'
+    six.innerHTML = '>>>'
+    eight.innerHTML = '7'
+    trace.innerHTML = "Davido"
+    el.src = "Davido-–-Timeless-Album.jpg";
+    music1.pause();
+    music1.currentTime = 0;
+    music2.pause();
+    music2.currentTime = 0;
+    music3.pause();
+    music3.currentTime = 0;
+    music4.pause();
+    music4.currentTime = 0;
+    music5.pause();
+    music5.currentTime = 0;
+    music6.play();
+}
+function seven() {
+    const el = document.querySelector('img')
+    const one = document.getElementById('one');
+    const two = document.getElementById('two');
+    const three = document.getElementById('three');
+    const four = document.getElementById('four');
+    const five = document.getElementById('five');
+    const six = document.getElementById('six');
+    const trace = document.getElementById('trace');
+    const now_playing1 = document.getElementById('now_playing1');
+    const not_playing = document.getElementById('not_playing');
+    const playlist_1 = document.getElementById('playlist_1');
+    const playlist_2 = document.getElementById('playlist_2');
+    const playlist_3 = document.getElementById('playlist_3');
+    const playlist_4 = document.getElementById('playlist_4');
+    const playlist_5 = document.getElementById('playlist_5');
+    const playlist_6 = document.getElementById('playlist_6');
+    const playlist_7 = document.getElementById('playlist_7');
+    now_playing1.innerHTML = "---";
+    not_playing.innerHTML = 'Now playing : ' + '---';
+    playlist_1.style.color = "white"
+    playlist_2.style.color = "white"
+    playlist_3.style.color = "white"
+    playlist_4.style.color = "white"
+    playlist_5.style.color = "white"
+    playlist_6.style.color = "white"
+    playlist_7.style.color = "red"
+    one.innerHTML = '1'
+    two.innerHTML = '2'
+    three.innerHTML = '3'
+    four.innerHTML = "4"
+    five.innerHTML = '5'
+    six.innerHTML = '6'
+    eight.innerHTML = '7'
+    trace.innerHTML = "Davido"
+    el.src = "Davido-–-Timeless-Album.jpg";
+    music1.pause();
+    music2.pause();
+    music3.pause();
+    music4.pause();
+    music5.pause();
+    music6.pause();
+}
+function push() {
+    const el = document.querySelector('img')
+    const one = document.getElementById('one');
+    const two = document.getElementById('two');
+    const three = document.getElementById('three');
+    const four = document.getElementById('four');
+    const five = document.getElementById('five');
+    const six = document.getElementById('six');
+    const eight = document.getElementById('eight');
+    const trace = document.getElementById('trace');
+    const now_playing1 = document.getElementById('now_playing1');
+    const not_playing = document.getElementById('not_playing');
+    const playlist_1 = document.getElementById('playlist_1');
+    const playlist_2 = document.getElementById('playlist_2');
+    const playlist_3 = document.getElementById('playlist_3');
+    const playlist_4 = document.getElementById('playlist_4');
+    const playlist_5 = document.getElementById('playlist_5');
+    const playlist_6 = document.getElementById('playlist_6');
+    const playlist_7 = document.getElementById('playlist_7');
+    const user_input = document.getElementById('user').value;
+    now_playing1.innerHTML = user_input;
+    not_playing.innerHTML = "Now playing : " + user_input + "-(UPCOMING ARTIST)";
+    playlist_1.style.color = "white"
+    playlist_2.style.color = "white"
+    playlist_3.style.color = "white"
+    playlist_4.style.color = "white"
+    playlist_5.style.color = "white"
+    playlist_6.style.color = "white"
+    playlist_7.style.color = "white"
+    one.style.color = "grey"
+    one.innerHTML = '1';
+    two.innerHTML = '2';
+    three.innerHTML = '3';
+    four.innerHTML = "4";
+    five.innerHTML = '5';
+    six.innerHTML = '6';
+    eight.innerHTML = '>>>';
+    trace.innerHTML = "UPCOMING ARTIST";
+    el.src = "pic - 3.jpeg";
+    music1.pause();
+    music2.pause();
+    music3.pause();
+    music4.pause();
+    music5.pause();
+    music6.pause();
+}
+function done() {
+    const removed = document.getElementById('added');
+    const trace = document.getElementById('trace');
+    const text = document.getElementById('text').value;
+    trace.innerHTML = text;
+    removed.innerHTML = " ";
+}
